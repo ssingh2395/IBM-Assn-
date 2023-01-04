1c316cd728c8eb446cdfbdc8fa68d379a8c07eeb
ssingh2395 <106338481+ssingh2395@users.noreply.github.com>
2023-01-04 16:23:56 -0500

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# Sarika Singh
+# Data Scientist 
+
+# I am interested in Data Science because I find that I am constantly learning and would like to continue to do so in my career. I enjoy the problem solving that comes with analyzing and visualizing data. 
+# 
+
+# ### The code below is supposed to be simple addition algebra. 
+
+# In[1]:
+
+
+1 + 1 
+
+
+# 1. Open Jupyter Notbook 
+# 2. write Code in Cell 
+# 3. Run the code 
